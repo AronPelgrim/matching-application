5b04525ca3477096bdf457da62459a4fde9294c9
AronPelgrim <aron.pelgrim@hva.nl>
2021-02-14 14:52:36 +0100

    Toevoeging ejs files

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const ejs = require('ejs');
 
-app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
 
-app.get('*', function(req, res){
-    res.send('Error 404', 404);
-  });
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.get('', (req, res) => {
+  res.render('index.ejs')
+})
 
 app.listen(PORT);
