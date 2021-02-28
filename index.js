ef93495c6bb1b1a1a46f18fe5c989d3ebde099af
AronPelgrim <aron.pelgrim@hva.nl>
2021-02-28 15:38:18 +0100

    eslint en prettier werkend hebben gemaakt

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+
 const app = express();
 const PORT = 3000;
 const ejs = require('ejs');
 
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.set('views', './views/pages')
-app.set('view engine', 'ejs')
+app.set('views', './views/pages');
+app.set('view engine', 'ejs');
 
 app.get('', (req, res) => {
-  res.render('index.ejs')
-})
+        res.render('index.ejs');
+});
 
 app.get('/matching', (req, res) => {
-  res.render('matching.ejs')
-})
+        res.render('matching.ejs');
+});
 
 app.post('/selection', urlencodedParser, (req, res) => {
-  console.log(req.body)
-  res.render('result')
-})
+        console.log(req.body);
+        res.render('result');
+});
 
-app.use(function (req, res, next) {
-  res.status(404).send("Sorry, deze pagina bestaat niet");
+app.use((req, res) => {
+        res.status(404).send('Sorry, deze pagina bestaat niet');
 });
 
 app.listen(PORT);
-//.env install in gitignore
