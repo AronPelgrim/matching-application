855c18eec5273db6ee83a0fb89dbfbb96aac0d21
AronPelgrim <aron.pelgrim@hva.nl>
2021-03-07 16:25:07 +0100

    Read songs is working with get req

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
 const router = express.Router();
-const path = require('path');
 const bodyParser = require('body-parser');
 
 const Nummers = require('../models/nummers');
@@ -11,28 +10,24 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
 router.get('', (req, res) => {
-        res.render('index.ejs');
+        res.render('index');
 });
 
 router.get('/matching', (req, res) => {
-        res.render('matching.ejs');
+        res.render('matching');
+});
+
+router.get('/result', (req, res) => {
+        Nummers.find().then((result) => {
+                res.render('result', { Nummers: result });
+        });
 });
 
 router.post('/index', (req, res) => {
         const nummer = new Nummers(req.body);
         nummer.save().then((result) => {
-                res.redirect('/');
+                res.redirect('result');
         });
-});
-
-router.get('/alleNummers', (req, res) => {
-        Nummers.find()
-                .then((result) => {
-                        res.send(result);
-                })
-                .catch((err) => {
-                        console.log(err);
-                });
 });
 
 router.use((req, res) => {
